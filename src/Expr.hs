c9dbf3e4a3a61b4f67558057f12041e53bb276a2
Hirochika Matsumoto <matsujika@gmail.com>
2020-04-25 05:46:18 +0900

    improvement: instantiate Expr on Show

--- src/Expr.hs
+++ src/Expr.hs
@@ -6,4 +6,10 @@ where
 data Expr
     = Var Char
     | Abs Expr Expr
-    | App Expr Expr deriving (Show)
+    | App Expr Expr
+
+instance Show Expr
+  where
+    show (Var x    ) = [x]
+    show (Abs x  t1) = "(\\" ++ show x ++ ". " ++ show t1 ++ ")"
+    show (App t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
